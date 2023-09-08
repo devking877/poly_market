4b865096cb100a9beb1e8bc3d33a6abfceae6786
devking877 <devking877@gmail.com>
2023-09-08 03:48:29 -0400

    nft_collection

--- pages/collections.tsx
+++ pages/collections.tsx
@@ -5,30 +5,36 @@ import { collections } from "../const/contractAddresses";
 import Link from "next/link";
 import styles from "../styles/Buy.module.css";
 
+type Props = {
+    collection:string;
+};
+  
+function GetCollections({collection} : Props) {
+    const {contract}=useContract(collection);
+    const {data: contractMetadata, isLoading}=useContractMetadata(contract);
+    return (
+        isLoading ? (
+            <div className={styles.nftContainer}>Loading</div>
+        ) : (
+            <Link href={`/buy/${collection}`} className={styles.nftContainer}>
+                <div>
+                    {contractMetadata?.name}<br/>
+                    {contractMetadata?.description}
+                </div>
+            </Link>
+        )
+    )
+}
+
 export default function Buy() {
-    let metaDatas : any[] = [];
-    collections.map((collection)=>{
-        const {contract}=useContract(collection);
-        const {data: contractMetadata, isLoading}=useContractMetadata(contract);
-        metaDatas.push(contractMetadata);
-    })
   return (
     <Container maxWidth="lg">
       <h1>Collections</h1>
         <div className={styles.nftGridContainer}>
             {
-                metaDatas.map((metadata, ind) => {
-                    // const {contract}=useContract(collection);
-                    // const {data: contractMetadata, isLoading}=useContractMetadata(contract);
-                    return (
-                        <Link href={`/buy/${collections[ind]}`} className={styles.nftContainer}>
-                            <div>
-                                {metadata?.name}<br/>
-                                {metadata?.description}
-                            </div>
-                        </Link>
-                    )
-                }
+                collections.map((collection) => (
+                    <GetCollections collection={collection}></GetCollections>
+                )
                 )
             }
         </div>
